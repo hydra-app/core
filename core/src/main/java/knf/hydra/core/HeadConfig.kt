d02d2fd821122c42f647e51a4277d8a25919b42c
Jordy Mendoza <jordyamc@hotmail.com>
2022-04-08 17:11:56 -0500

    Update documentation: - All files documented
    Changed all references of chapter to content in order to generalise the API:
    ChaptersData.kt -> ContentData.kt
    ChapterModel.kt -> ContentItemModel.kt
    
    Simplified some methods:
    - Remove SearchModel.kt, instead use DirectoryModel.kt because it has the same structure
    - Renamed UserProfileModel.kt to ProfileModel.kt

--- core/src/main/java/knf/hydra/core/HeadConfig.kt
+++ core/src/main/java/knf/hydra/core/HeadConfig.kt
@@ -1,3 +1,9 @@
+/*
+ * Created by @UnbarredStream on 08/04/22 17:11
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 08/04/22 17:10
+ */
+
 package knf.hydra.core
 
 import knf.hydra.core.models.DirectoryModel
